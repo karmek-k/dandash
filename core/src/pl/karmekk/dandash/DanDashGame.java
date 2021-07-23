330478918586300fc36a9b097132c9810b78401c
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-07-23 20:17:14 +0200

    Place the player entity on the screen

--- core/src/pl/karmekk/dandash/DanDashGame.java
+++ core/src/pl/karmekk/dandash/DanDashGame.java
@@ -1,19 +1,32 @@
 package pl.karmekk.dandash;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import pl.karmekk.dandash.entities.Player;
 
 public class DanDashGame extends ApplicationAdapter {
+    private SpriteBatch batch;
+    private Player player;
+
     @Override
     public void create() {
+        batch = new SpriteBatch();
+        player = new Player();
     }
 
     @Override
     public void render() {
         ScreenUtils.clear(0.2f, 0.2f, 0.2f, 1f);
+
+        batch.begin();
+        player.draw(batch);
+        batch.end();
     }
 
     @Override
     public void dispose() {
+        batch.dispose();
+        player.dispose();
     }
 }
