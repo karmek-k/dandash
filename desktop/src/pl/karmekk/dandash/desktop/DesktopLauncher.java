bf620c59b6a0b4d6e8ca0cb7460473ffcd5fedf9
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-07-23 19:21:52 +0200

    Reindent java files to 4 spaces

--- desktop/src/pl/karmekk/dandash/desktop/DesktopLauncher.java
+++ desktop/src/pl/karmekk/dandash/desktop/DesktopLauncher.java
@@ -5,16 +5,16 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import pl.karmekk.dandash.DanDashGame;
 
 public class DesktopLauncher {
-	public static final int WIDTH = 800;
-	public static final int HEIGHT = 600;
+    public static final int WIDTH = 800;
+    public static final int HEIGHT = 600;
 
-	public static void main (String[] arg) {
-		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+    public static void main (String[] arg) {
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 
-		config.setTitle("DanDash");
-		config.setWindowedMode(WIDTH, HEIGHT);
-		config.setResizable(false);
+        config.setTitle("DanDash");
+        config.setWindowedMode(WIDTH, HEIGHT);
+        config.setResizable(false);
 
-		new Lwjgl3Application(new DanDashGame(), config);
-	}
+        new Lwjgl3Application(new DanDashGame(), config);
+    }
 }
