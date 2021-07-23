6c23afc47c34e522e5871a9ab152df6b9458bcee
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-07-23 19:08:33 +0200

    Initial commit

---
+++ desktop/src/pl/karmekk/dandash/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package pl.karmekk.dandash.desktop;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import pl.karmekk.dandash.DanDashGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		new Lwjgl3Application(new DanDashGame(), config);
+	}
+}
