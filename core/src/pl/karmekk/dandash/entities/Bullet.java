dc16bd2d6e37b101987d8a2cc063ab55c6580a7b
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-07-26 13:42:48 +0200

    Implement linear acceleration for Bullet

--- core/src/pl/karmekk/dandash/entities/Bullet.java
+++ core/src/pl/karmekk/dandash/entities/Bullet.java
@@ -1,18 +1,28 @@
 package pl.karmekk.dandash.entities;
 
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.TimeUtils;
 
 public class Bullet extends BaseEntity {
     private final Vector2 velocity;
+    private final Vector2 linearAcceleration;
+    private long initialTime;
 
-    public Bullet(int x, int y, Vector2 velocity) {
+    public Bullet(int x, int y, Vector2 velocity, Vector2 linearAcceleration) {
         super(x, y);
         this.velocity = velocity;
+        this.linearAcceleration = linearAcceleration;
+
+        this.initialTime = TimeUtils.millis();
     }
 
     @Override
     public void move() {
-        this.rect.x += velocity.x;
-        this.rect.y += velocity.y;
+        this.rect.x += calculateSpeed(velocity.x, linearAcceleration.x) * dt();
+        this.rect.y += calculateSpeed(velocity.y, linearAcceleration.y) * dt();
+    }
+
+    private float calculateSpeed(float velocityVal, float accelerationVal) {
+        return velocityVal + accelerationVal * TimeUtils.timeSinceMillis(initialTime);
     }
 }
