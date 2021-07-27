84ea290345518f039b6a59f375eb2dbb31a00c42
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-07-27 16:37:37 +0200

    Implement a rough player shooting function

--- core/src/pl/karmekk/dandash/DanDashGame.java
+++ core/src/pl/karmekk/dandash/DanDashGame.java
@@ -2,15 +2,18 @@ package pl.karmekk.dandash;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
 import pl.karmekk.dandash.entities.Player;
 import pl.karmekk.dandash.entities.Drawable;
+import pl.karmekk.dandash.entities.projectiles.Bullet;
 
 public class DanDashGame extends ApplicationAdapter {
     private SpriteBatch batch;
     private Player player;
     private Array<Drawable> drawables;
+    private Array<Bullet> bullets;
 
     @Override
     public void create() {
@@ -20,6 +23,8 @@ public class DanDashGame extends ApplicationAdapter {
 
         drawables = new Array<>();
         drawables.add(player);
+
+        bullets = new Array<>();
     }
 
     @Override
@@ -27,6 +32,24 @@ public class DanDashGame extends ApplicationAdapter {
         ScreenUtils.clear(0.2f, 0.2f, 0.2f, 1f);
 
         player.move();
+
+        // TODO: extract this to a separate method
+        if (player.isShooting(100)) {
+            Bullet bullet = new Bullet(player.getX(), player.getY(), new Vector2(0, 500));
+            bullets.add(bullet);
+            drawables.add(bullet);
+        }
+        for (Bullet b : bullets) {
+            if (b.offScreen()) {
+                drawables.removeValue(b, true);
+                bullets.removeValue(b, true);
+                b.dispose();
+
+                continue;
+            }
+
+            b.move();
+        }
 
         batch.begin();
 
