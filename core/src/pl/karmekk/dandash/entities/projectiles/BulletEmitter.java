80ad476d2f2c97508bc51c949908f55176bf9b0e
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-07-29 16:36:45 +0200

    Create BulletEmitter abstract class

--- core/src/pl/karmekk/dandash/entities/projectiles/BulletEmitter.java
+++ core/src/pl/karmekk/dandash/entities/projectiles/BulletEmitter.java
@@ -1,10 +1,27 @@
 package pl.karmekk.dandash.entities.projectiles;
 
+import pl.karmekk.dandash.entities.BaseEntity;
+
 /**
  * An object that can shoot bullets.
  */
-public interface BulletEmitter {
-    boolean isShooting();
+public abstract class BulletEmitter extends BaseEntity {
+    private long shootDelay;
 
-    void setShootDelay(long shootDelay);
+    /**
+     * Builds a new entity that has the given rectangle and a default texture.
+     *
+     * @param x X coordinate of the bottom left vertex.
+     * @param y Y coordinate of the bottom left vertex.
+     */
+    public BulletEmitter(int x, int y, long shootDelay) {
+        super(x, y);
+        this.shootDelay = shootDelay;
+    }
+
+    public abstract boolean isShooting();
+
+    void setShootDelay(long shootDelay) {
+        this.shootDelay = shootDelay;
+    }
 }
